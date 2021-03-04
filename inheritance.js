ef007f0f329bbf80e2605518ce3e139a66244f3b
Ashwini <ashwinikedari@live.com>
2021-03-04 18:53:46 +0530

    inheritance using class added

--- inheritance.js
+++ inheritance.js
@@ -1,3 +1,4 @@
+/**INHERITANCE USING PROTOTYPE START */
 // Create an object called Teacher derived from the Person class,
 // and implement a method called teach which receives a string called subject, and prints out:
 var Person = function(){};
@@ -31,3 +32,49 @@ var sir = new Teacher();
 sir.initialize("Dixit", 57);
 sir.teach("OOJS");
 
+/**INHERITANCE USING PROTOTYPE END */
+
+/**INHERITANCE USING CLASS START */
+
+class Person1 {
+    constructor(first, last, age, gender, interests){
+        this.name = {
+            first,
+            last
+        };
+        this.age = age;
+        this.gender = gender;
+        this.interests = interests
+    }
+
+    greeting(){
+        console.log(`Hi, i am ${this.name.first}`)
+    }
+
+    farewell(){
+        console.log(`${this.name.first} has left the building. Bye for now!`)
+    }
+}
+
+let han = new Person1('Han', 'Solo', 25, 'male', ['Smuggling']);
+han.greeting();
+
+let leia = new Person1('Leia', 'Organa', 19, 'female', ['Government']);
+leia.farewell();
+
+
+class Teacher1 extends Person1{
+    constructor(first, last, age, gender, interests, subject, grade){
+        super(first, last, age, gender, interests);
+        this.subject = subject;
+        this.grade = grade;
+    }
+}
+
+let snape = new Teacher1('Severus', 'Snape', 58, 'male', ['Potions'], 'Dark arts', 5);
+snape.greeting();
+snape.farewell();
+console.log(snape.age);
+console.log(snape.subject);
+
+/**INHERITANCE USING CLASS END */
